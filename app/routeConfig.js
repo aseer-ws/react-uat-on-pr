e687f791e66e604b91067e3a6c21338092dc1343
Aseer KT <aseer@wednesday.is>
2022-03-08 15:16:53 +0530

    chore: simplify uat script

--- app/routeConfig.js
+++ app/routeConfig.js
@@ -1,6 +1,8 @@
+import React from 'react';
 import NotFound from '@containers/NotFoundPage/Loadable';
 import HomeContainer from '@containers/HomeContainer/Loadable';
 import routeConstants from '@utils/routeConstants';
+
 export const routeConfig = {
   repos: {
     component: HomeContainer,
--- uat/index.js
+++ uat/index.js
@@ -1,42 +1,36 @@
-// uat/index.js
-export async function detectRedirect() {
+export async function redirect() {
   let pathname = window.location.pathname;
 
-  if (['/', ''].includes(pathname)) {
-    if (pathname.includes('/index.html')) {
-      pathname = pathname.replace('/index.html', '');
-    }
-    await getSPAHTML();
-  } else {
+  if (['', '/', '/index.html'].includes(pathname)) {
     return;
   }
+  if (pathname.includes('/index.html')) {
+    pathname = pathname.replace('/index.html', '');
+  }
 
-  async function getSPAHTML() {
-    let newPathname = pathname;
+  const pathnames = pathname.split('/').filter(val => val !== '');
+  let loopCount = pathnames.length;
 
-    while (newPathname) {
-      // remove empty strings in the edges after split
-      let pathnames = newPathname.split('/').filter(val => val !== '');
-      let updatedPathname = '';
-      // construct updatedPathname by stripping off last entry in pathnames array
-      // eg: if pathnames = ['feat', 'uat', 'repos'], then updatedPathname = '/feat/uat'
-      for (let i = 0; i < pathnames.length - 1; i++) {
-        updatedPathname += `/${pathnames[i]}`;
-      }
-      if (!updatedPathname) {
-        window.location.assign(window.location.origin + '/index.html');
-        return;
-      }
-      let updatedUrl = window.location.origin + updatedPathname;
-      const res = await fetch(updatedUrl);
-      if (res.ok) {
-        window.location.assign(updatedUrl + '/index.html' + `?redirect_uri=${pathname.replace(updatedPathname, '')}`);
-        return;
-      } else {
-        newPathname = updatedPathname;
-      }
+  // /feat/uat/tracks/123
+
+  // 4 => 3,2,1
+
+  for (let k = loopCount - 1; k > 0; k--) {
+    // remove empty strings in the edges after split
+    let newPathname = '';
+    for (let i = 0; i < k; i++) {
+      newPathname += `/${pathnames[i]}`;
+    }
+
+    let updatedUrl = window.location.origin + newPathname;
+    const res = await fetch(updatedUrl);
+    if (res.ok) {
+      window.location.assign(updatedUrl + '/index.html' + `?redirect_uri=${pathname.replace(newPathname, '')}`);
+      return;
     }
   }
+
+  window.location.assign(window.location.origin + '/index.html');
 }
 
-detectRedirect();
+redirect();
