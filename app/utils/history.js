5fb145df3248bb02b25b92ea0d85082d80728157
Aseer KT <aseer@wednesday.is>
2022-03-08 16:43:40 +0530

    trial: check uat 5

--- app/utils/history.js
+++ app/utils/history.js
@@ -1,25 +1,10 @@
 import { createBrowserHistory } from 'history';
-import routeConstants from '@utils/routeConstants';
 import { isUAT } from './index';
-const routes = Object.keys(routeConstants);
-let pathname = window.location.pathname;
-// let pathname = window.location.pathname.replace('/index.html', '');
+// let pathname = window.location.pathname;
+let pathname = window.location.pathname.replace('/index.html', '');
 let baseUrl = '';
 if (isUAT()) {
-  let isMatchedOnce = false;
-  routes.forEach(routeKey => {
-    const route = routeConstants[routeKey].route;
-    if (pathname.endsWith(route)) {
-      baseUrl = pathname.substring(0, pathname.length - route.length);
-      isMatchedOnce = true;
-    } else if (pathname.endsWith(`${route}/`)) {
-      baseUrl = pathname.substring(0, pathname.length - route.length - 1);
-      isMatchedOnce = true;
-    }
-  });
-  if (!isMatchedOnce) {
-    baseUrl = pathname;
-  }
+  baseUrl = pathname;
 }
 
 console.log({ baseUrl, isUAT: isUAT(), pathname });
